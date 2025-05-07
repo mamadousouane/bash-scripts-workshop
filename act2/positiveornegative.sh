fced06d71ff3f697c31e8d5bea5ca729f68e1acb
user <user@icesat-mamadou.Home>
2025-05-07 16:30:39 +0200

    Added

---
+++ act2/positiveornegative.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Enter an integer: " num
+
+if [ $num -gt 0 ]; then
+    echo "The number is positive."
+elif [ $num -lt 0 ]; then
+    echo "The number is negative."
+else
+    echo "The number is zero."
+fi
